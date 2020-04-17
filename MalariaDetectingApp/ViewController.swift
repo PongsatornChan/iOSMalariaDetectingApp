dfbb441e0870d12ca163606c3e2d9128f5753fd7
Pongsatorn Chanpanichravee <pongsatorn1612@berkeley.edu>
2020-04-16 19:17:20 -0700

    Initial Commit

---
+++ MalariaDetectingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MalariaDetectingApp
+//
+//  Created by Mark Chanpanichravee on 4/16/20.
+//  Copyright © 2020 Mark Chanpanichravee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
