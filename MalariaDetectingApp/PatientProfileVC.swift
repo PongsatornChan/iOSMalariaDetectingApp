b9dc62d9dfabceca60d1db39fbe03e24ab5d5a49
Pongsatorn Chanpanichravee <pongsatorn1612@berkeley.edu>
2020-05-02 00:48:56 -0700

    Basic Done

--- MalariaDetectingApp/PatientProfileVC.swift
+++ MalariaDetectingApp/PatientProfileVC.swift
@@ -9,42 +9,43 @@
 import Foundation
 import UIKit
 
-class PatientProfileVC: UIViewController {
+class PatientProfileVC: UIViewController,
+        UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var genderLabel: UILabel!
-    @IBOutlet weak var resultsTableView: UITableView!
-
+    @IBOutlet weak var profileTableView: UITableView!
+    @IBOutlet weak var testButton: UIButton!
+    
     var patientIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let patient = patientsModel.patientLst[patientIndex]
-        print("in Profile \(patient.resultCount())\n")
         profileImageView.image = patient.image
         nameLabel.text = "\(patient.firstname) \(patient.lastname)"
-        ageLabel.text = "\(patient.age)"
-        genderLabel.text = patient.gender
+        ageLabel.text = (patient.age < 0) ? "" : "Age: \(patient.age)"
+        genderLabel.text = (patient.gender == "") ? "" : "Gender: \(patient.gender)"
+        
+        profileTableView.delegate = self
+        profileTableView.dataSource = self
+        
+        // this function messes the Tab bar controller
+        testButton.isHidden = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        print("viewWillAppear ProfileVC.\(patientsModel.patientLst[patientIndex].resultCount())\n")
         reloadInputViews()
-        resultsTableView.reloadData()
+        profileTableView.reloadData()
     }
     
     @IBAction func testButtonPressed(_ sender: UIButton) {
         performSegue(withIdentifier: "profileToTestSegue", sender: sender)
     }
     
-}
-
-extension PatientProfileVC: UITableViewDelegate, UITableViewDataSource {
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let patient = patientsModel.patientLst[patientIndex]
-        print("Patient have \(patient.results.resultCount()) results.\n")
         return patient.results.resultCount()
     }
     
@@ -54,19 +55,17 @@ extension PatientProfileVC: UITableViewDelegate, UITableViewDataSource {
         let image = patient.results.imageLst[index]
         let pResult = patient.results.pResults[index]
         let uResult = patient.results.uResults[index]
-        print("In profile table view \n")
         
-        if let cell = resultsTableView.dequeueReusableCell(withIdentifier: "resultTableCell", for: indexPath) as? ResultTableViewCell {
+        if let cell = profileTableView.dequeueReusableCell(withIdentifier: "profileTableCell", for: indexPath) as? ProfileTableViewCell {
             
             print("In profile make new cell")
-            cell.resultImageView.image = image
+            cell.cellImageView.image = image
             cell.parasitLabel.text = "Parasitized: \(pResult) %"
-            cell.UninfectLabel.text = "Uninfected: \(uResult) %"
+            cell.uninfectLabel.text = "Uninfected: \(uResult) %"
             
             return cell
         } else {
             return UITableViewCell()
         }
     }
-    
 }
---
+++ MalariaDetectingApp/ProfileTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileTableViewCell.swift
+//  MalariaDetectingApp
+//
+//  Created by Mark Chanpanichravee on 5/2/20.
+//  Copyright © 2020 Mark Chanpanichravee. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var cellImageView: UIImageView!
+    @IBOutlet weak var parasitLabel: UILabel!
+    @IBOutlet weak var uninfectLabel: UILabel!
+}
